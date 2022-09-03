8a03ce5085fd6fabe550bf2c95052186655c9958
Maryelin <mayeat94@gmail.com>
2022-09-03 12:04:03 -0400

    practicing saturday

---
+++ 03_strings.py
@@ -0,0 +1,24 @@
+# Los strings son cadenas de letras
+# Se pueden hacer con "" o ''
+
+str_one = "Micro"
+str_two = 'System'
+
+# Juntar uno o más strings se denomina concatenar. En python
+# concatenar utilizando el operador '+'
+print(str_one + str_two)
+
+# Ijo: con la ',' la función print No
+#concatena
+
+
+
+
+
+
+
+ print('hello'*3)
+
+ str_three = 'bye'
+
+ result = str_three * 5
